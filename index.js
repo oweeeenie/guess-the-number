6d55eac93fe298db384d692d0adf9e4a80160efa
oweeeenie <owensullivan419@yahoo.com>
2024-08-12 00:46:35 -0400

    styling majorly done, everything is aligned

---
+++ index.js
@@ -0,0 +1,8 @@
+const guessButtonContainer = document.createElement("div");
+guessButtonContainer.id = "guess-button-container";
+
+const guessButton = document.createElement("button");
+guessButton.id = "guess-button";
+
+document.body.appendChild(guessButtonContainer)
+guessButtonContainer.appendChild(guessButton);
