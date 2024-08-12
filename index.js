03793bb5cb533caeb1e7c97351593b0e3a35d0c7
oweeeenie <owensullivan419@yahoo.com>
2024-08-12 15:09:00 -0400

    design done, need bit more functionality

--- index.js
+++ index.js
@@ -6,3 +6,24 @@ guessButton.id = "guess-button";
 
 document.body.appendChild(guessButtonContainer)
 guessButtonContainer.appendChild(guessButton);
+
+const targetNumber = Math.floor(Math.random() * 100) + 1;
+
+// start of functionality
+const textBox = document.querySelector("#text-box");
+const messageElement = document.querySelector("#message");
+const submitButton = document.querySelector("#submit-button");
+
+    submitButton.addEventListener('click' , () => {
+        const userInput = Number(textBox.value);
+    
+    if (isNaN(userInput) || userInput === '') {
+        messageElement.textContent = '';
+    } else if (userInput < targetNumber) {
+        messageElement.textContent = 'Higher';
+    } else if (userInput > targetNumber) {
+        messageElement.textContent = 'Lower';
+    } else if (userInput === targetNumber) {
+        messageElement.textContent = 'Correct!';
+    } 
+});
